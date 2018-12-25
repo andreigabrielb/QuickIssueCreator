557bde03502f724f2ce9b5a9d4859ed2522cc238
andrei.bochis@gmail.com <andrei.bochis@gmail.com>
2018-12-25 12:36:04 +0200

    Added the tester name label and text box

--- QuickIssueCreatorMain.py
+++ QuickIssueCreatorMain.py
@@ -35,16 +35,25 @@ class QIC_Main_UI:
 
         #Define and initialize a text box field in which the project version can be written
         self.project_version_label = Label(master, text = "Project version: ")
-        self.project_version_label.grid(row = 2)
+        self.project_version_label.grid(row = 1, column = 2)
 
         self.project_version_text = Text(master, height = 1, width = 30)
         self.project_version_text.insert(END, "project version no.")
-        self.project_version_text.grid(row = 2, column = 1)
+        self.project_version_text.grid(row = 1, column = 3)
+
+        #Define and initialize a text box field in which the project version can be written
+        self.tester_name_label = Label(master, text = "Tester name: ")
+        self.tester_name_label.grid(row = 2)
+
+        self.tester_name_text = Text(master, height = 1, width = 30)
+        self.tester_name_text.insert(END, "tester name")
+        self.tester_name_text.grid(row = 2, column = 1)
 
     #this function defines what happens when a project is selected from project Optionmenu
     def project_option_function(self, value):
         print value
         print self.project_version_text.get("1.0", "end-1c")
+        print self.tester_name_text.get("1.0", "end-1c")
 
 #initialization of the application
 if __name__ == "__main__":
