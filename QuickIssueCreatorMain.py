c3526e9956189dcf85d365d85c520a600ed35df6
AndreiGabrielB <andrei.bochis@gmail.com>
2019-01-16 15:09:40 +0200

    Added button to load ao create files for issue tracking and also the functionlaty for those buttons. Create message box notification pop up for action completed tasks.

--- QuickIssueCreatorMain.py
+++ QuickIssueCreatorMain.py
@@ -9,6 +9,7 @@ from Tkinter import OptionMenu
 from Tkinter import StringVar
 from Tkinter import Text
 from Tkinter import END
+from tkFileDialog import askopenfilename
 
 #This class will create the main application UI as an object with all of the buttons it requires
 class QIC_Main_UI:
@@ -20,6 +21,9 @@ class QIC_Main_UI:
         
         #define the created issues index variable
         self.issue_index = 0
+
+        #ser the value of the issue_file_name under use to ""
+        self.issue_file_name = ''
 
         #Project defintion label and text field
         self.project_label = Label(master, text = "Project: ")
@@ -64,11 +68,19 @@ class QIC_Main_UI:
         self.issue_file_name_text.insert(END, "File_name.txt")
         self.issue_file_name_text.grid(row = 3, column = 1)
 
+        #create button to load an existing issues file
+        self.create_issues_file_button = Button (master, text = "Create issues file", command = lambda: self.create_new_issue_file(self.issue_file_name_text.get("1.0", "end-1c")))
+        self.create_issues_file_button.grid(row = 3, column = 2)
+
+        #create button to load an existing issues file
+        self.load_issues_file_button = Button (master, text = "Load issues file", command = lambda: self.load_issue_file())
+        self.load_issues_file_button.grid(row = 3, column = 3)
+
         #button that will call a Toplevel UI to create a new issue
-        self.create_issue_button = Button(master, text = "Create issue", command = self.create_issue_method)
+        self.create_issue_button = Button(master, text = "Create issue", command = lambda: self.create_issue_method())
         self.create_issue_button.grid(row = 10)
 
-         #Application close button initialization 
+        #Application close button initialization 
         self.close_app_button = Button(master, text = "Close application", command = master.quit)
         self.close_app_button.grid(row = 10, column = 5)
     
@@ -94,7 +106,7 @@ class QIC_Main_UI:
         issue_description_text.grid(row = 2, column = 1)
 
         #define the button that will save a created issue
-        save_close_issue_button = Button(create_issue_window, text = "Save & Close", command = lambda: self.save_issue_method(create_issue_window))
+        save_close_issue_button = Button(create_issue_window, text = "Save", command = lambda: self.save_issue_method(create_issue_window))
         save_close_issue_button.grid(row = 5)
 
         #define the button that will discard an issue and close the window
@@ -112,6 +124,37 @@ class QIC_Main_UI:
         print self.project_version_text.get("1.0", "end-1c")
         print self.tester_name_text.get("1.0", "end-1c")
         print self.tested_HW_text.get("1.0", "end-1c")
+
+    #this method will create a new file with the received namea and make that the active file name under use
+    def create_new_issue_file(self, name):
+        #Assign the value from the issue_file_name_text to the issue_file_name object variable as the active file under use
+        if name == '':
+            print"no file name"
+            self.message_box_notification("No file name given.")
+        else:
+            self.issue_file_name = name
+            f = open(self.issue_file_name, "w+")
+            f.write("This is a test file")
+            f.close()
+            self.message_box_notification("File %s created" % self.issue_file_name)
+
+    # this method will load an existng issue file as the issue file under use
+    def load_issue_file(self):
+        #This function will load a file and assign it to the appropriate object variable
+        self.issue_file_name = askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+        self.message_box_notification("File %s loaded" % self.issue_file_name)
+
+    # this method will create a message box and print the passed text
+    def message_box_notification(self, txt):
+        #crate the top level for message box
+        mess_box = Toplevel()
+        mess_box.title("Message")
+        #create the message text
+        msg = Message(mess_box, text = txt)
+        msg.pack()
+        #create top level close button
+        mess_box_close = Button(mess_box, text = "OK", command = mess_box.destroy)
+        mess_box_close.pack()
 
 #initialization of the application
 if __name__ == "__main__":
